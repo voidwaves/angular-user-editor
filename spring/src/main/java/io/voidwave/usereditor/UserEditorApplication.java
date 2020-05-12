91de05ed0ea41e2f88567bd11fb7c80e28388bbe
HellwigJ <j.hellwig@eos-ts.com>
2020-05-12 14:14:45 +0200

    added spring boilerplate

---
+++ spring/src/main/java/io/voidwave/usereditor/UserEditorApplication.java
@@ -0,0 +1,13 @@
+package io.voidwave.usereditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEditorApplication.class, args);
+	}
+
+}
